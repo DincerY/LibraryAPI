5a72be017cd8ceb92f6a1ed6c9f61e24484ec09d
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-08-01 15:58:37 +0300

    .

---
+++ Presentation/LibraryAPI.API/Extensions/ConfigureExceptionHandlerExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace LibraryAPI.API.Extensions
+{
+    public static class ConfigureExceptionHandlerExtension
+    {
+        public static void ConfigureExceptionHandler(this WebApplication application)
+        {
+            application.UseExceptionHandler(builder =>
+            {
+                builder.Run(async context =>
+                {
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(JsonSerializer.Serialize(new
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message,
+                            Title = "Hata Alındı"
+                        }));
+                    }
+                });
+            });
+        }
+
+    }
+}
