6a70b2d3cccc74fc63a757c87620ffda4b3b2b55
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-08-23 17:01:36 +0300

    .

--- Core/LibraryAPI.Application/Features/Books/Commands/Create/CreatedBookResponse.cs
+++ Core/LibraryAPI.Application/Features/Books/Commands/Create/CreatedBookResponse.cs
@@ -8,9 +8,5 @@ namespace LibraryAPI.Application.Features.Books.Commands.Create;
 
 public class CreatedBookResponse
 {
-    public string Title { get; set; }
-    public string PageNumber { get; set; }
-    public string Description { get; set; }
-    public string[]? AuthorId { get; set; }
-    public string[]? LibraryId { get; set; }
+
 }
